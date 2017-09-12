950b0c5c82e70fffdd8853e4b325c750cd11088d
Valentine1 <devnetdot@gmail.com>
2017-09-12 18:22:16 +0300

    dungeon and teleport

---
+++ Assets/Project/Scripts/Game/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public Teleporter exitTeleporter;
+    public float exitOffset = 4.0f;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Player>() != null)
+        {
+            if (exitTeleporter != null)
+            {
+                Player player = col.GetComponent<Player>();
+                player.transform.position = exitTeleporter.transform.position + exitTeleporter.transform.forward * exitOffset;
+            }
+        }
+    }
+}
